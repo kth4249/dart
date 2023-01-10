ef85d751e42cdaf1608544441b63f64f353da789
kth4249 <desales4249@gmail.com>
2023-01-10 18:02:51 +0900

    Class, Constuctor

---
+++ Constructors.dart
@@ -0,0 +1,22 @@
+class Player {
+  // late final String name;
+  // late int xp;
+  final String name;
+  int xp;
+  // Player(String name, int xp) {
+  //   this.name = name;
+  //   this.xp = xp;
+  // }
+  Player(this.name, this.xp);
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+void main() {
+  var player = Player("nico", 1500);
+  player.sayHello();
+  var player2 = Player("lynn", 2500);
+  player2.sayHello();
+}
